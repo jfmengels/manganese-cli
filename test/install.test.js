a39e84693f39bf753eca753d9079dbc307c7cf39
Jeroen Engels <jfm.engels@gmail.com>
2015-08-05 00:04:07 +0200

    Add list option for install
    Adding --list when calling the istall subcommand will find and list all the available manganese plugins on npm.

--- test/install.test.js
+++ test/install.test.js
@@ -33,7 +33,7 @@ describe('install', function() {
             install.installCore(plugins)
             .then(function() {
                 expect(coreStub.callCount).to.equal(1);
-                // expect(result).to.deep.equal(['some-plugin', 'some-other-plugin']);
+                expect(coreStub.getCall(0).args[0]).to.deep.equal(plugins);
                 done();
             })
             .catch(done);
@@ -59,15 +59,68 @@ describe('install', function() {
         });
     });
 
+    describe('list', function() {
+        let findPluginsStub;
+
+        beforeEach(function() {
+            findPluginsStub = sinon.stub(core.installer, 'findPlugins');
+            findPluginsStub.yields(null, ['some-plugin', 'some-other-plugin']);
+        });
+
+        afterEach(function() {
+            core.installer.findPlugins.restore();
+        });
+
+        it('should return a Promise', function() {
+            expect(install.list().then).to.be.a('function');
+        });
+
+        it('should call manganese.installer.findPlugins', function(done) {
+            install.list()
+            .then(function() {
+                expect(findPluginsStub.callCount).to.equal(1);
+                expect(findPluginsStub.getCall(0).args).to.have.length(1);
+                done();
+            })
+            .catch(done);
+        });
+
+        it('should resolve to the list of found plugins', function(done) {
+            install.list()
+            .then(function(result) {
+                expect(result).to.deep.equal(['some-plugin', 'some-other-plugin']);
+                done();
+            })
+            .catch(done);
+        });
+
+        it('should reject when manganese.installer.findPlugins fails', function(done) {
+            const expectedError = new Error('some-error with findPlugins');
+            findPluginsStub.onFirstCall().yields(expectedError);
+            install.list()
+            .catch(function(error) {
+                expect(error.message).to.equal(expectedError.message);
+                done();
+            })
+            .catch(done);
+        });
+    });
+
     describe('parseArgs', function () {
-        let installCoreStub, configAddPluginsStub;
-        let args;
+        let installCoreStub, listStub, configAddPluginsStub;
+        let args, options;
 
         beforeEach(function() {
             args = ['some-plugin ', 'some-other-plugin'];
+            options = {
+                verbose: 'normal'
+            };
 
             installCoreStub = sinon.stub(install, 'installCore');
             installCoreStub.resolves(args);
+
+            listStub = sinon.stub(install, 'list');
+            listStub.resolves(args);
 
             configAddPluginsStub = sinon.stub(config, 'addPlugins');
             configAddPluginsStub.resolves();
@@ -75,62 +128,101 @@ describe('install', function() {
 
         afterEach(function() {
             install.installCore.restore();
+            install.list.restore();
             config.addPlugins.restore();
         });
 
-        it('should return a Promise', function() {
-            expect(install.parseArgs(args).then).to.be.a('function');
+        describe('with options.list', function() {
+            beforeEach(function() {
+                options.list = true;
+            });
+
+            it('should return a Promise', function() {
+                expect(install.parseArgs(args, options).then).to.be.a('function');
+            });
+
+            it('should call install.list', function(done) {
+                install.parseArgs(args, options)
+                .then(function() {
+                    expect(listStub.callCount).to.equal(1);
+                    expect(listStub.getCall(0).args[0]).to.equal(options);
+                    done();
+                })
+                .catch(done);
+            });
+
+            it('should reject when install.list fails', function(done) {
+                const expectedError = new Error('some error with list');
+                listStub.onFirstCall().rejects(expectedError);
+                install.parseArgs(args, options)
+                .catch(function(error) {
+                    expect(error.message).to.equal(expectedError.message);
+                    done();
+                })
+                .catch(done);
+            });
+
         });
 
-        it('should reject when there args is not defined', function(done) {
-            install.parseArgs()
-            .catch(function(error) {
-                expect(error.message).to.equal('no plugins were specified');
-                done();
-            })
-            .catch(done);
-        });
+        describe('without options.list', function() {
+            beforeEach(function() {
+                delete options.list;
+            });
 
-        it('should reject when there args is an empty array', function(done) {
-            install.parseArgs([])
-            .catch(function(error) {
-                expect(error.message).to.equal('no plugins were specified');
-                done();
-            })
-            .catch(done);
-        });
+            it('should return a Promise', function() {
+                expect(install.parseArgs(args, options).then).to.be.a('function');
+            });
 
-        it('should call install.installCore', function(done) {
-            install.parseArgs(args)
-            .then(function() {
-                expect(installCoreStub.callCount).to.equal(1);
-                expect(installCoreStub.getCall(0).args[0]).to.equal(args);
-                done();
-            })
-            .catch(done);
-        });
+            it('should reject when there args is not defined', function(done) {
+                install.parseArgs(null, options)
+                .catch(function(error) {
+                    expect(error.message).to.equal('no plugins were specified');
+                    done();
+                })
+                .catch(done);
+            });
 
-        it('should reject when install.installCore fails', function(done) {
-            const expectedError = new Error('some error with installCore');
-            installCoreStub.onFirstCall().rejects(expectedError);
-            install.parseArgs(args)
-            .catch(function(error) {
-                expect(error.message).to.equal(expectedError.message);
-                done();
-            })
-            .catch(done);
-        });
+            it('should reject when there args is an empty array', function(done) {
+                install.parseArgs([], options)
+                .catch(function(error) {
+                    expect(error.message).to.equal('no plugins were specified');
+                    done();
+                })
+                .catch(done);
+            });
 
-        it('should save the newly installed plugins in the default config', function(done) {
-            install.parseArgs(args)
-            .then(function() {
-                expect(configAddPluginsStub.callCount).to.equal(1);
-                const addPluginsArgs = configAddPluginsStub.getCall(0).args;
-                expect(addPluginsArgs).to.have.length(1);
-                expect(addPluginsArgs[0]).to.deep.equal(args);
-                done();
-            })
-            .catch(done);
+            it('should call install.installCore', function(done) {
+                install.parseArgs(args, options)
+                .then(function() {
+                    expect(installCoreStub.callCount).to.equal(1);
+                    expect(installCoreStub.getCall(0).args[0]).to.equal(args);
+                    done();
+                })
+                .catch(done);
+            });
+
+            it('should reject when install.installCore fails', function(done) {
+                const expectedError = new Error('some error with installCore');
+                installCoreStub.onFirstCall().rejects(expectedError);
+                install.parseArgs(args, options)
+                .catch(function(error) {
+                    expect(error.message).to.equal(expectedError.message);
+                    done();
+                })
+                .catch(done);
+            });
+
+            it('should save the newly installed plugins in the default config', function(done) {
+                install.parseArgs(args, options)
+                .then(function() {
+                    expect(configAddPluginsStub.callCount).to.equal(1);
+                    const addPluginsArgs = configAddPluginsStub.getCall(0).args;
+                    expect(addPluginsArgs).to.have.length(1);
+                    expect(addPluginsArgs[0]).to.deep.equal(args);
+                    done();
+                })
+                .catch(done);
+            });
         });
     });
 });
